7f41f1f78106e754561d1dca55448a882a0c8407
Aroon Valdez <rv23-1099@unphu.edu.do>
2024-01-25 10:58:25 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void agregarcontacto(String nombre){
+
+    }
+    public static void removerContacto(int id){
+
+    }
+    public static String actualizarcontacto(int A){
+        return "N/A";}
+
+    public static void mostrarContacto(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
