e02cedc7b28c3c71b6c29f46d55c683f98f915d8
aroon <rv23-1099@unphu.edu.do>
2024-02-15 19:36:59 -0400

    Segunda version

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
-    public static void agregarcontacto(String nombre){
+    public static void agregarcontacto(){
+        String contacto;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un contacto a agregar ");
+        contacto = sc.nextLine();
+
+        if ( contacto.length() > 3 && contacto.length() < 9 ){
+
+            System.out.println("Contacto agregado con exito");
+
+        } else if (contacto.length() > 7 && contacto.length() < 31) {
+
+            System.out.println("Agregado, contiene entre 8-30 caracteres");
+
+        } else {
+
+            System.out.println("Nombre muy largo");
+
+        }
+
 
     }
     public static void removerContacto(int id){
+
+
+        if (id >1000 && id <9999){
+
+            System.out.println("Verificando contacto a eliminar...");
+
+        } else{
+
+           System.out.println("ID inválido");
+        }
 
     }
     public static String actualizarcontacto(int A){
@@ -12,9 +42,16 @@ public class Main {
 
     public static void mostrarContacto(){
 
+        System.out.println("Mostrando contacto");
+
     }
 
     public static void main(String[] args) {
+
+        agregarcontacto();
+       // removerContacto();
+        //mostrarContacto();
+
 
     }
 }
